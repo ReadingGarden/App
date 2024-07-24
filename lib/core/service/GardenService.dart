212672c663baa0959ab5b894c19b8cc5d6160dad
so0nim <308go@naver.com>
2024-07-24 15:19:32 +0900

    [FEAT] api 추가

--- lib/core/service/GardenService.dart
+++ lib/core/service/GardenService.dart
@@ -121,12 +121,58 @@ class GardenService {
     }
   }
 
+  //가든 책 이동
+  Future<Response?> moveToGarden(int garden_no, int to_garden_no) async {
+    final accessToken = await loadAccess();
+    try {
+      final response = await _dio.put(
+          '${Constant.URL}garden/to?garden_no=$garden_no&to_garden_no=$to_garden_no',
+          options: Options(headers: {'Authorization': 'Bearer $accessToken'}));
+      print(response.data.toString());
+      return response;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        // 서버가 응답한 경우 (상태 코드와 함께)
+        print('Error: ${e.response?.data}');
+        print('Status code: ${e.response?.statusCode}');
+        return e.response;
+      } else {
+        // 서버가 응답하지 않은 경우
+        print('Error sending request: ${e.message}');
+        return null;
+      }
+    }
+  }
+
   //가든 대표 변경
   Future<Response?> putGardenLeader(int garden_no, int user_no) async {
     final accessToken = await loadAccess();
     try {
       final response = await _dio.put(
           '${Constant.URL}garden/member?garden_no=$garden_no&user_no=$user_no',
+          options: Options(headers: {'Authorization': 'Bearer $accessToken'}));
+      print(response.data.toString());
+      return response;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        // 서버가 응답한 경우 (상태 코드와 함께)
+        print('Error: ${e.response?.data}');
+        print('Status code: ${e.response?.statusCode}');
+        return e.response;
+      } else {
+        // 서버가 응답하지 않은 경우
+        print('Error sending request: ${e.message}');
+        return null;
+      }
+    }
+  }
+
+  //가든 메인 변경
+  Future<Response?> putGardenMain(int garden_no) async {
+    final accessToken = await loadAccess();
+    try {
+      final response = await _dio.put(
+          '${Constant.URL}garden/main?garden_no=$garden_no',
           options: Options(headers: {'Authorization': 'Bearer $accessToken'}));
       print(response.data.toString());
       return response;
