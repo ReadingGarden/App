f38b4c6c789fded7f4851bf31faa096391c7eeb7
so0nim <308go@naver.com>
2025-01-05 13:39:41 +0900

    feat: 읽기 시작 날짜 미래 금지

--- lib/utils/AutoInputFormatter.dart
+++ lib/utils/AutoInputFormatter.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class AutoInputFormatter extends TextInputFormatter {
   @override
@@ -20,6 +21,43 @@ class AutoInputFormatter extends TextInputFormatter {
           newText.substring(4, 6) +
           '.' +
           newText.substring(6, min(8, newText.length));
+    }
+
+    // // Check if the formatted text represents a valid date
+    // DateTime? inputDate;
+    // try {
+    //   inputDate = DateFormat('yyyy.MM.dd').parseStrict(newText);
+    // } catch (e) {
+    //   inputDate = null;
+    // }
+
+    // // Get today's date
+    // final today = DateTime.now();
+
+    // // If the input date is in the future, set it to today
+    // if (inputDate != null && inputDate.isAfter(today)) {
+    //   newText = DateFormat('yyyy.MM.dd').format(today);
+    // }
+
+    // Only validate and adjust the date if the input length matches 'yyyy.MM.dd'
+    if (newText.length == 10) {
+      DateTime? inputDate;
+      final today = DateTime.now();
+
+      try {
+        inputDate = DateFormat('yyyy.MM.dd').parseStrict(newText);
+
+        // If the date is in the future, replace it with today's date
+        if (inputDate.isAfter(today)) {
+          inputDate = today;
+        }
+      } catch (e) {
+        // If parsing fails, replace with today's date
+        inputDate = today;
+      }
+
+      // Format the final date as yyyy.MM.dd
+      newText = DateFormat('yyyy.MM.dd').format(inputDate);
     }
 
     // Ensure the cursor remains at the end of the text
