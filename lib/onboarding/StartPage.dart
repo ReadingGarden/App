e09cb45554d84cce370c51d98db51980bfa47d0f
so0nim <308go@naver.com>
2025-01-05 14:01:55 +0900

    feat: 하단 인디케이터 페이지뷰에서 제거

--- lib/onboarding/StartPage.dart
+++ lib/onboarding/StartPage.dart
@@ -5,29 +5,76 @@ import 'package:go_router/go_router.dart';
 import '../utils/AppColors.dart';
 import '../utils/Widgets.dart';
 
-class StartPage extends StatelessWidget {
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  final PageController _pageController = PageController();
+  final List titleList = [
+    '내가 읽고 있는 책,\n독서가든에서 키워볼까?',
+    '친구와 함께 키워나가는\n우리의 공유 가든',
+    '내가 읽는 책이 모여서\n꽃을 피워낼때까지'
+  ];
+  final List contentList = [
+    '하루하루 꾸준히 책을 보다보면\n내가 읽은 책이 꽃이 되어 가든을 가득 채울거에요',
+    '함께 공유 가든에 나무를 심으면 더 풍성해져요. \n그리고 어쩌면 친구의 책을 따라 읽게 될지도?',
+    '독서는 꾸준함이 생명이죠.\n까먹지 않도록 원하는 때에 알림을 보내드릴게요!'
+  ];
+
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
-            shrinkWrap: true,
-            physics: const PageScrollPhysics(),
-            scrollDirection: Axis.horizontal,
-            itemCount: 3,
-            itemBuilder: (context, index) {
-              return (index == 0)
-                  ? startContainer()
-                  : (index == 1)
-                      ? start2Container()
-                      : start3Container();
-            }),
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: 3,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return startContainer(index);
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(bottom: 30.h),
+              child: SizedBox(
+                  width: 34.w,
+                  height: 6.h,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    children: List.generate(3, (int index) {
+                      return Container(
+                        padding: EdgeInsets.only(right: 8.w),
+                        child: CircleAvatar(
+                          radius: 3.r,
+                          backgroundColor: (index == _currentIndex)
+                              ? AppColors.primaryColor
+                              : AppColors.grey_CA,
+                        ),
+                      );
+                    }),
+                  )),
+            )
+          ],
+        ),
         bottomNavigationBar: Container(
           margin: EdgeInsets.only(bottom: 32.h, left: 24.w, right: 24.w),
           child: Widgets.button('시작하기', true, () => context.goNamed('login')),
         ));
   }
 
-  Widget startContainer() {
+  Widget startContainer(int index) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -46,7 +93,7 @@ class StartPage extends StatelessWidget {
                     Padding(
                       padding: EdgeInsets.only(bottom: 8.h),
                       child: Text(
-                        '내가 읽고 있는 책,\n독서가든에서 키워볼까?',
+                        titleList[index],
                         style: TextStyle(
                           fontSize: 24.sp,
                           fontWeight: FontWeight.w600,
@@ -54,7 +101,7 @@ class StartPage extends StatelessWidget {
                       ),
                     ),
                     Text(
-                      '하루하루 꾸준히 책을 보다보면\n내가 읽은 책이 꽃이 되어 가든을 가득 채울거에요',
+                      contentList[index],
                       style:
                           TextStyle(fontSize: 14.sp, color: AppColors.grey_8D),
                     ),
@@ -62,159 +109,6 @@ class StartPage extends StatelessWidget {
                 )),
           ],
         ),
-        Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.only(bottom: 30.h),
-          width: 320.w,
-          child: SizedBox(
-            width: 34.w,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CircleAvatar(
-                  radius: 3.r,
-                  backgroundColor: AppColors.primaryColor,
-                ),
-                CircleAvatar(
-                  radius: 3.r,
-                  backgroundColor: AppColors.grey_CA,
-                ),
-                CircleAvatar(
-                  radius: 3.r,
-                  backgroundColor: AppColors.grey_CA,
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget start2Container() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          children: [
-            Container(
-              width: 360.w,
-              height: 400.h,
-              color: Colors.red,
-            ),
-            Container(
-                margin: EdgeInsets.only(top: 30.h),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 8.h),
-                      child: Text(
-                        '친구와 함께 키워나가는\n우리의 공유 가든',
-                        style: TextStyle(
-                          fontSize: 24.sp,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      '함께 공유 가든에 나무를 심으면 더 풍성해져요. \n그리고 어쩌면 친구의 책을 따라 읽게 될지도?',
-                      style:
-                          TextStyle(fontSize: 14.sp, color: AppColors.grey_8D),
-                    ),
-                  ],
-                )),
-          ],
-        ),
-        Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.only(bottom: 30.h),
-          width: 320.w,
-          child: SizedBox(
-            width: 34.w,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CircleAvatar(
-                  radius: 3.r,
-                  backgroundColor: AppColors.grey_CA,
-                ),
-                CircleAvatar(
-                  radius: 3.r,
-                  backgroundColor: AppColors.primaryColor,
-                ),
-                CircleAvatar(
-                  radius: 3.r,
-                  backgroundColor: AppColors.grey_CA,
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget start3Container() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          children: [
-            Container(
-              width: 360.w,
-              height: 400.h,
-              color: Colors.amber,
-            ),
-            Container(
-                margin: EdgeInsets.only(top: 30.h),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 8.h),
-                      child: Text(
-                        '내가 읽는 책이 모여서\n꽃을 피워낼때까지',
-                        style: TextStyle(
-                          fontSize: 24.sp,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      '독서는 꾸준함이 생명이죠.\n까먹지 않도록 원하는 때에 알림을 보내드릴게요!',
-                      style:
-                          TextStyle(fontSize: 14.sp, color: AppColors.grey_8D),
-                    ),
-                  ],
-                )),
-          ],
-        ),
-        Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.only(bottom: 30.h),
-          width: 320.w,
-          child: SizedBox(
-            width: 34.w,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CircleAvatar(
-                  radius: 3.r,
-                  backgroundColor: AppColors.grey_CA,
-                ),
-                CircleAvatar(
-                  radius: 3.r,
-                  backgroundColor: AppColors.grey_CA,
-                ),
-                CircleAvatar(
-                  radius: 3.r,
-                  backgroundColor: AppColors.primaryColor,
-                ),
-              ],
-            ),
-          ),
-        )
       ],
     );
   }
